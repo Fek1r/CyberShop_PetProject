95a5499dcc6a0b6918ee217d328c7fb28e5754c4
octolss <jeremejev1vladimir@gmail.com>
2024-11-08 10:41:01 +0200

    Creating header, home, footer directories + setting header

---
+++ src/header/header.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import './header.css';
+
+
+function Header(){
+    return (
+        <div className="returner">
+            <div className="header">
+                <div className="social-networks">
+                    <img src='youtube.png'></img>
+                    <img src='youtube.png'></img>
+                    <img src='youtube.png'></img>
+                </div>
+                <div className="logo">
+
+                </div>
+                <div className="links">
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+export default Header;
