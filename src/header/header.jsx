f627e1261baca36d626cb42ed8a0721133058bba
octolss <jeremejev1vladimir@gmail.com>
2024-11-08 14:39:56 +0200

    creating header

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import './header.css';
 
 
@@ -7,15 +7,21 @@ function Header(){
         <div className="returner">
             <div className="header">
                 <div className="social-networks">
-                    <img src='youtube.png'></img>
-                    <img src='youtube.png'></img>
-                    <img src='youtube.png'></img>
+                    <a href=''><div className="image2"></div></a>
+                    <a href=''><div className="image"></div></a>
+                    <a href=''><div className="image1"></div></a>
                 </div>
                 <div className="logo">
-
+                    <h1>Baltic industrial park</h1>
                 </div>
                 <div className="links">
-
+                    <ul className='links-components'>
+                        <li><Link to="/projekti">Projekti</Link></li>
+                        <li><Link to="/projekti">Jaunumi</Link></li>
+                        <li><Link to="/projekti">Ilgtspējība</Link></li>
+                        <li><Link to="/projekti">Par mums</Link></li>
+                        <li><Link to="/projekti">Kontakti</Link></li>
+                    </ul>
                 </div>
             </div>
         </div>
