e2121869b35c449fa592b93acfa195ae3cc9999a
Fek1r <stivkivi@gmail.com>
2024-11-14 10:29:44 +0200

    Fix Head

--- src/head/head.jsx
+++ src/head/head.jsx
@@ -1,37 +1,44 @@
 import { Link } from 'react-router-dom';
 import './head.css';
+import { useEffect } from 'react';
 
-function Head(){
-    return (
-        
-        <div className="return">
-            <style>
-                @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
-            </style>
-            <div className="head">
-                <div className="logotype-n">
-                    <Link to="/">
-                        <div className="logotype">
-                        </div>
-                    
-                    <p>BALTIJAS INDUSTRIĀLAIS PARKS</p>
-                    </Link>
-                </div>
-                <div className="ul-elements">
-                    <ul className="links-components">
-                        <li><Link to="/projects">Projekti</Link></li>
-                        <li><Link to="/new-jau">Jaunumi</Link></li>
-                        <li><Link to="/projekti">Ilgtspējība</Link></li>
-                        <li><Link to="/about">Par Mums</Link></li>
-                    </ul>
-                </div>
-                <div class="contact-button">
-                    <div class="circle"></div>
-                    <button><Link to="/contact">Contact Us</Link></button>
-                </div>
-            </div>
+function Head() {
+  useEffect(() => {
+    const header = document.getElementById('header');
+
+    window.onscroll = function () {
+      if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+      } else {
+        header.classList.remove('scrolled');
+      }
+    };
+  }, []);
+
+  return (
+    <div className="return">
+      <div id="header" className="head">
+        <div className="logotype-n">
+          <Link to="/">
+            <div className="logotype"></div>
+            <p>BALTIJAS INDUSTRIĀLAIS PARKS</p>
+          </Link>
         </div>
-    );
+        <div className="ul-elements">
+          <ul className="links-components">
+            <li><Link to="/projects">Projekti</Link></li>
+            <li><Link to="/new-jau">Jaunumi</Link></li>
+            <li><Link to="/projekti">Ilgtspējība</Link></li>
+            <li><Link to="/about">Par Mums</Link></li>
+          </ul>
+        </div>
+        <div className="contact-button">
+          <div className="circle"></div>
+          <button><Link to="/contact">Contact Us</Link></button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Head;
